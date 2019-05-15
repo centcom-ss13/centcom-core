63401924a6704704aa85fe8ece028a27d4f19faa
Steven Kneisler <stevenkneisler@gmail.com>
2019-05-14 23:40:48 -0400

    Set temporary config in cert
    What is security again?

--- compose.js
+++ compose.js
@@ -2,22 +2,30 @@ const shell = require('shelljs');
 const config = require('config');
 
 
+const safeConfigGet = (key) => {
+  try {
+    return config.get(key);
+  } catch(e) {
+    return undefined;
+  }
+};
+
 const environment = {
-  CENTCOM_API_SSL: config.get('apiSSL'),
-  CENTCOM_API_PORT: config.get('apiPort'),
-  CENTCOM_API_HOST: config.get('apiHost'),
-  CENTCOM_DEBUG: config.get('debug'),
-  CENTCOM_DB_DATABASE: config.get('databaseDb'),
-  CENTCOM_DB_HOST: config.get('databaseUrl'),
-  CENTCOM_DB_PASSWORD: config.get('databasePassword'),
-  CENTCOM_DB_PORT: config.get('databasePort'),
-  CENTCOM_DB_USER: config.get('databaseUsername'),
-  CENTCOM_FRONT_END_PORT: config.get('frontEndPort'),
-  CENTCOM_FRONT_END_SSL: config.get('frontEndSSL'),
-  CENTCOM_FRONT_END_URL: config.get('frontEndUrl'),
-  CENTCOM_SSL_CERT_FILE: config.get('apiSSLCertFile'),
-  CENTCOM_SSL_KEY_FILE: config.get('apiSSLKeyFile'),
-  CENTCOM_SSL_KEY_PASSPHRASE: config.get('apiSSLKeyPassphrase'),
+  CENTCOM_API_SSL: safeConfigGet('apiSSL'),
+  CENTCOM_API_PORT: safeConfigGet('apiPort'),
+  CENTCOM_API_HOST: safeConfigGet('apiHost'),
+  CENTCOM_DEBUG: safeConfigGet('debug'),
+  CENTCOM_DB_DATABASE: safeConfigGet('databaseDb'),
+  CENTCOM_DB_HOST: safeConfigGet('databaseUrl'),
+  CENTCOM_DB_PASSWORD: safeConfigGet('databasePassword'),
+  CENTCOM_DB_PORT: safeConfigGet('databasePort'),
+  CENTCOM_DB_USER: safeConfigGet('databaseUsername'),
+  CENTCOM_FRONT_END_PORT: safeConfigGet('frontEndPort'),
+  CENTCOM_FRONT_END_SSL: safeConfigGet('frontEndSSL'),
+  CENTCOM_FRONT_END_URL: safeConfigGet('frontEndUrl'),
+  CENTCOM_SSL_CERT_FILE: safeConfigGet('apiSSLCertFile'),
+  CENTCOM_SSL_KEY_FILE: safeConfigGet('apiSSLKeyFile'),
+  CENTCOM_SSL_KEY_PASSPHRASE: safeConfigGet('apiSSLKeyPassphrase'),
 };
 
 Object.entries(environment).forEach(([key, value]) => {
