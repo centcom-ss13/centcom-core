a4404b96e6fd91461580c87aa2b18b768620eb29
Steven Kneisler <stevenkneisler@gmail.com>
2019-05-16 01:46:23 -0400

    Modify CERT api host url for docker test

--- config/cert.js
+++ config/cert.js
@@ -2,7 +2,7 @@ module.exports = {
   databaseUrl: 'mysql',
   databaseUsername: 'root',
   databasePassword: 'wowItsMeAPassword',
-  apiHost: '134.209.42.179',
+  apiHost: '127.0.0.1',
   apiPort: 3000,
   frontEndUrl: '134.209.42.179',
   frontEndPort: 80,
